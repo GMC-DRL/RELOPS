8b2c4abc247ba6d54d13e7dcdaf89f55064880a9
zhan-jiajun <2906433289@qq.com>
2025-03-19 21:35:50 +0800

    Add the Augmented_WCCI2020_Dataset

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,5 @@
 from problem import bbob, bbob_torch, protein_docking
-
+from problem.MTO.customize.Augmented_WCCI2020_numpy.Augmented_WCCI2020_numpy_dataset import Augmented_WCCI2020_Dataset
 
 def construct_problem_set(config):
     problem = config.problem
@@ -23,5 +23,11 @@ def construct_problem_set(config):
                                                                     train_batch_size=config.train_batch_size,
                                                                     test_batch_size=config.test_batch_size,
                                                                     difficulty=config.difficulty)
+    
+    elif problem in ['augmented-WCCI2020']:
+        return Augmented_WCCI2020_Dataset.get_datasets(dim=config.dim,
+                                                       train_batch_size=config.train_batch_size,
+                                                       test_batch_size=config.test_batch_size,
+                                                       difficulty=config.difficulty)
     else:
         raise ValueError(problem + ' is not defined!')
